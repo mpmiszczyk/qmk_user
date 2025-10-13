56114de5252602c181977259a1cc313bcf19c5dd
mpmiszczyk <mpmiszczyk@gmail.com>
2025-10-13 19:19:13 +0200

    feat: flake setup

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: with nixpkgs.legacyPackages.x86_64-linux; {
+
+    # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+    devShells.x86_64-linux.default = mkShell {
+      packages = [
+        qmk
+        keymapviz
+      ];
+    };
+  };
+}
